0f0d66ce8d1031d0a066c7fc2fd766c306a55351
vuwase <v.uwase@alustudent.com>
2025-10-19 07:07:48 -0700

    Add task 6 multiple customer insertions

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,3 @@
--- Task 6: Insert multiple rows into the customer table
--- Database: alx_book_store
-
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
